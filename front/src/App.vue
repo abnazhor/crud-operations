de8144f13a5e0b4e0098c40fd6c5694fc7ac3a36
abnazhor <jorbar982@outlook.com>
2021-05-17 01:23:28 +0200

    First commit. Add CRUD table and main view

---
+++ front/src/App.vue
@@ -0,0 +1,224 @@
+<template>
+  <div class="h-screen flex items-center justify-center bg-gray-100">
+    <CrudDisplay :data="test" />
+  </div>
+</template>
+
+<script>
+import CrudDisplay from "./components/CrudDisplay.vue";
+
+const test = [
+  {
+    key: 1,
+    value: 3232,
+    value2: 3232,
+  },
+  {
+    key: 2,
+    value: 324234,
+    value2: 3232,
+  },
+  {
+    key: 3,
+    value: 123423,
+    value2: 3232,
+  },
+  {
+    key: 4,
+    value: 115,
+    value2: 3232,
+  },
+  {
+    key: 4,
+    value: 115,
+    value2: 3232,
+  },
+  {
+    key: 4,
+    value: 115,
+    value2: 3232,
+  },
+  {
+    key: 4,
+    value: 115,
+    value2: 3232,
+  },
+  {
+    key: 4,
+    value: 115,
+    value2: 3232,
+  },
+  {
+    key: 4,
+    value: 115,
+    value2: 3232,
+  },
+  {
+    key: 4,
+    value: 115,
+    value2: 3232,
+  },
+  {
+    key: 4,
+    value: 115,
+    value2: 3232,
+  },
+  {
+    key: 4,
+    value: 115,
+    value2: 3232,
+  },
+  {
+    key: 4,
+    value: 115,
+    value2: 3232,
+  },
+  {
+    key: 4,
+    value: 115,
+    value2: 3232,
+  },
+  {
+    key: 4,
+    value: 115,
+    value2: 3232,
+  },
+  {
+    key: 4,
+    value: 115,
+    value2: 3232,
+  },
+  {
+    key: 4,
+    value: 115,
+    value2: 3232,
+  },
+  {
+    key: 4,
+    value: 115,
+    value2: 3232,
+  },
+  {
+    key: 4,
+    value: 115,
+    value2: 3232,
+  },
+  {
+    key: 4,
+    value: 115,
+    value2: 3232,
+  },
+  {
+    key: 4,
+    value: 115,
+    value2: 3232,
+  },
+  {
+    key: 4,
+    value: 115,
+    value2: 3232,
+  },
+  {
+    key: 4,
+    value: 115,
+    value2: 3232,
+  },
+  {
+    key: 4,
+    value: 115,
+    value2: 3232,
+  },
+  {
+    key: 4,
+    value: 115,
+    value2: 3232,
+  },
+  {
+    key: 4,
+    value: 115,
+    value2: 3232,
+  },
+  {
+    key: 4,
+    value: 115,
+    value2: 3232,
+  },
+  {
+    key: 4,
+    value: 115,
+    value2: 3232,
+  },
+  {
+    key: 4,
+    value: 115,
+    value2: 3232,
+  },
+  {
+    key: 4,
+    value: 115,
+    value2: 3232,
+  },
+  {
+    key: 4,
+    value: 115,
+    value2: 3232,
+  },
+  {
+    key: 4,
+    value: 115,
+    value2: 3232,
+  },
+  {
+    key: 4,
+    value: 115,
+    value2: 3232,
+  },
+  {
+    key: 4,
+    value: 115,
+    value2: 3232,
+  },
+  {
+    key: 4,
+    value: 115,
+    value2: 3232,
+  },
+  {
+    key: 4,
+    value: 115,
+    value2: 3232,
+  },
+  {
+    key: 4,
+    value: 115,
+    value2: 3232,
+  },
+  {
+    key: 4,
+    value: 115,
+    value2: 3232,
+  },
+  {
+    key: 4,
+    value: 115,
+    value2: 3232,
+  },
+  {
+    key: 4,
+    value: 115,
+    value2: 3232,
+  }
+];
+
+export default {
+  name: "App",
+  components: {
+    CrudDisplay,
+  },
+  data() {
+    return {
+      test,
+    };
+  },
+};
+</script>
---
+++ front/src/components/CrudDisplay.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="h-4/6 bg-white shadow-sm rounded-md px-5 pt-5 w-8/12 h-5/6">
+    <div id="toolbar" class="p-2 lg:flex-row flex mb-6 flex-col">
+      <button
+        class="border py-1 px-5 rounded-md mx-1 flex items-center justify-center font-light"
+      >
+        <i class="ri-add-line text-2xl mr-1"></i>
+        Agregar
+      </button>
+      <button
+        class="border py-1 px-5 rounded-md mx-1 flex items-center justify-center font-light"
+      >
+        <i class="ri-delete-bin-line ri-add-line text-xl mr-1"></i>
+        Eliminar
+      </button>
+      <button
+        class="border py-1 px-5 rounded-md mx-1 flex items-center justify-center font-light"
+      >
+        <i class="ri-arrow-left-right-line ri-add-line text-xl mr-1"></i>
+        Modificar
+      </button>
+    </div>
+    <div id="display-table" class="overflow-x-hidden lg:h-5/6 scrollbar-thin px-3 h-4/6">
+      <table class="w-full h-full">
+        <tr v-for="item in data" :key="item.key">
+          <td class="w-12 border">
+            <input type="checkbox" name="" id="" class="h-full m-auto block" />
+          </td>
+          <td class="p-2 border">
+            {{ item.value }}
+          </td>
+          <td class="p-2 border">
+            {{ item.value2 }}
+          </td>
+        </tr>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CrudDisplay",
+  props: {
+    data: Array,
+  },
+};
+</script>
